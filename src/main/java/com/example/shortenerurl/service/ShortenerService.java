290b848b2c198cd054a0a3a81bcf43320611b50d
Oséias <oseas-ramos123@hotmail.com>
2023-07-30 00:16:06 -0300

    [BACK-#feat] first commit

---
+++ src/main/java/com/example/shortenerurl/service/ShortenerService.java
@@ -0,0 +1,56 @@
+package com.example.shortenerurl.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import com.example.shortenerurl.controller.ShortenerController;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShortenerService {
+
+    private HashMap<String, String> urlMap = new HashMap<>();
+    Map<String, Integer> counters = new HashMap<>();
+
+    private static final String ALLOWED_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int SHORT_URL_LENGTH = 6;
+
+    public String longToShort(String longUrl) {
+
+        // Criando uma string vazia para armazenar o resultado
+        StringBuilder shortUrlBuilder = new StringBuilder();
+
+        // Gerando a sequência de caracteres do short URL
+        Random random = new Random(System.currentTimeMillis());
+        for (int i = 0; i < SHORT_URL_LENGTH; i++) {
+            int randomIndex = random.nextInt(ALLOWED_CHARACTERS.length());
+            char randomChar = ALLOWED_CHARACTERS.charAt(randomIndex);
+            shortUrlBuilder.append(randomChar);
+        }
+
+        // Convertendo o StringBuilder para String final
+        String shortUrl = shortUrlBuilder.toString();
+
+        // Armazenando a relação entre a URL curta (shortUrl) e a URL longa (longUrl) no mapa
+        urlMap.put(shortUrl, longUrl);
+
+        return shortUrl;
+    }
+
+
+
+    public String shortToLong(String shortUrl) {
+
+        String response = urlMap.get(shortUrl);
+
+        if(response != null) {
+
+            counters.put(shortUrl, counters.getOrDefault(shortUrl, 0) + 1);
+        }
+        return response;
+    }
+    public Integer getCount(String shortUrl){
+        return counters.get(shortUrl);
+    }
+}
